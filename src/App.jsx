e98ab1eee2c23de532707d6dde51fd102008d674
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-15 15:54:47 -0300

    chore: init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+import './global.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Vite</h1>
+      <Header />
+    </div>
+  )
+}
+
+export default App
