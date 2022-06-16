d43c199fd46e7ef2daeb5492b0695dffb7bbfee8
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-15 21:20:51 -0300

    feat: adding Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { Header } from './components/Header'
+import { Header, Sidebar } from './components'
+
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
   return (
     <div>
-      <h1>Hello Vite</h1>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div>
+            <h1>Luciano Souza</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              saepe!
+            </p>
+          </div>
+          <div>
+            <h1>Luciano Souza</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              saepe!
+            </p>
+          </div>
+          <div>
+            <h1>Luciano Souza</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              saepe!
+            </p>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        src='https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=20'
+        alt='Profile Banner'
+        className={styles.cover}
+      />
+      <div className={styles.profile}>
+        <img
+          src='https://github.com/lucianodiisouza.png'
+          alt='Profile picture'
+          className={styles.avatar}
+        />
+        <strong>Luciano dii Souza</strong>
+        <span>Frontend Engineer</span>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
+
+export default Sidebar
